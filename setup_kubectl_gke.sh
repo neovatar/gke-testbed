306ff15dae85936705935193618c443305631892
Thomas Seliger <tom@neovatar.org>
2018-12-07 17:23:58 +0100

    Script for downloading kubectl config

---
+++ setup_kubectl_gke.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+##
+## This script creates a config file for accessing an gke cluster
+## with kubectl. It uses gcloud cli to retrieve the needed information
+##
+
+CLUSTER_NAME="gke-test"
+KUBECONFIG="${HOME}/.kube/config-${CLUSTER_NAME}"
+
+# make sure kubectl config dir exists
+mkdir -p "$(dirname "${KUBECONFIG}")"
+
+# call gloud get-credentials
+gcloud container clusters get-credentials "${CLUSTER_NAME}"
+
+echo
+echo "You can use the created kubeconfig with"
+echo "export KUBECONFIG=\"${KUBECONFIG}\""
+echo
--- terraform/gke/terraform-backend.tf
+++ terraform/gke/terraform-backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket  = "moonlit-oven-221217-tfstate"
-    prefix  = "terraform/state/eks"
+    prefix  = "terraform/state/gke"
     region  = "europe-west3"
     project = "moonlit-oven-221217"
   }
